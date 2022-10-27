adc2186a6d59011b07c3511719c44e7e8babd83a
unknown <almen.elias@gmail.com>
2022-10-27 14:36:37 +0200

    breadcrumbs working

--- src/components/BreadCrumbsRouterComponent.js
+++ src/components/BreadCrumbsRouterComponent.js
@@ -1,29 +1,19 @@
 import React from 'react'
-import { useMatches } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
+import useBreadcrumbs from "use-react-router-breadcrumbs";
 
-function Breadcrumbs() {
-  let matches = useMatches();
-  let crumbs = matches
-    // first get rid of any matches that don't have handle and crumb
-    .filter((match) => Boolean(match.handle?.crumb))
-    // now map them into an array of elements, passing the loader
-    // data to each one
-    .map((match) => match.handle.crumb(match.data));
-
-  return (
-    <ol>
-      {crumbs.map((crumb, index) => (
-        <li key={index}>{crumb}</li>
-      ))}
-    </ol>
-  );
-}
 
 const BreadCrumbsRouterComponent = () => {
-  
-  return (
-    <div className="breadcrumbs-section container">{Breadcrumbs}</div>
-  )
-}
+  const breadcrumbs = useBreadcrumbs();
 
+  return (
+    <>
+      {breadcrumbs.map(({ match, breadcrumb }) => (
+        <NavLink key={match.pathname} to={match.pathname}>
+          {breadcrumb}
+        </NavLink>
+      ))}
+    </>
+  );
+};
 export default BreadCrumbsRouterComponent
--- src/views/ContactsView.js
+++ src/views/ContactsView.js
@@ -3,11 +3,13 @@ import FormSection from '../sections/FormSection'
 import FooterSection from '../sections/FooterSection'
 import HeaderSection from '../sections/HeaderSection'
 import MapSection from '../sections/MapSection'
+import BreadCrumbsSection from '../sections/BreadCrumbsSection'
 
 const ContactsView = () => {
   return (
     <>
       <HeaderSection />
+      <BreadCrumbsSection />
       <MapSection />
       <FormSection />
       <FooterSection />
