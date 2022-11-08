211f4c905a381b2f78ca3fc264891e3cd5f9ebfe
unknown <almen.elias@gmail.com>
2022-11-08 11:10:22 +0100

    before related products

--- src/views/ProductDetailsView.js
+++ src/views/ProductDetailsView.js
@@ -26,6 +26,57 @@ const ProductDetailsView = () => {
     fetchDetails()
   }, [setData]);
 
+
+  //TABS
+  // https://reactjsexample.com/react-simple-tabs-made-with-an-array-of-objects/
+  const [currentTab, setCurrentTab] = useState('1');
+    const tabs = [
+        {
+            id: 1,
+            tabTitle: 'Description',
+            contentRow0: 'Way extensive and dejection get delivered deficient sincerity gentleman age. Too end instrument possession contrasted motionless. Calling offence six joy feeling. Coming merits and was talent enough far. Sir joy northward sportsmen education. Discovery incommode earnestly no he commanded if. Put still any about manor heard.',
+            contentRow1: '* Village did removed enjoyed explain nor ham saw calling talking.',
+            contentRow2: '* Securing as informed declared or margaret.',
+            contentRow3: '* Joy horrible moreover man feelings own shy.',
+            contentRow4: 'On even feet time have an no at. Relation so in confined smallest children unpacked delicate. Why sir end believe uncivil respect. Always get adieus nature day course for common. My little garret repair to desire he esteem. '
+        },
+        {
+            id: 2,
+            tabTitle: 'Additional',
+            contentRow0: 'zero',
+            contentRow1: 'one',
+            contentRow2: 'tow',
+            contentRow3: 'three',
+            contentRow4: 'four'
+        },
+        {
+            id: 3,
+            tabTitle: 'Shopping & Returns',
+            contentRow0: 'a',
+            contentRow1: 'b',
+            contentRow2: 'c',
+            contentRow3: 'd',
+            contentRow4: 'e'
+        },
+        {
+            id: 4,
+            tabTitle: 'Reviews',
+            contentRow0: '0',
+            contentRow1: '1',
+            contentRow2: '2',
+            contentRow3: '3',
+            contentRow4: '4'
+        }
+    ];
+
+    const handleTabClick = (e) => {
+        setCurrentTab(e.target.id);
+    }
+
+
+
+
+
   return (
     <>
       <HeaderSection />
@@ -72,8 +123,19 @@ const ProductDetailsView = () => {
               </div>
             </div>
           </div>
-          <div className="text-tabs">
-
+          <div className="text-tabs mt-5">
+            <div className='tabs'>
+                  {tabs.map((tab, i) =>
+                      <button key={i} id={tab.id} disabled={currentTab === `${tab.id}`} onClick={(handleTabClick)}>{tab.tabTitle}</button>
+                  )}
+              </div>
+              <div className='content'>
+                  {tabs.map((tab, i) =>
+                      <div key={i}>
+                          {currentTab === `${tab.id}` && <div><p className='mt-3'>{tab.contentRow0}</p><p>{tab.contentRow1}</p><p>{tab.contentRow2}</p><p>{tab.contentRow3}</p><p>{tab.contentRow4}</p></div>}
+                      </div>
+                  )}
+              </div>
           </div>
           <div className="related-products">
 
